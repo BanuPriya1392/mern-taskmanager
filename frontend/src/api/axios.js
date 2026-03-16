37bf49a6664d71273d1d3556bcada2e30aaf2d39
BanuPriya1392 <banusangee@gmail.com>
2026-03-16 20:17:59 +0530

    PERSONAL TASK MANAGER-MERN

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+import axios from "axios";
+//axios instance with base url and interceptor to add token to headers if it exists in local storage
+const API = axios.create({ baseURL: "http://localhost:5000/api" });
 
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
