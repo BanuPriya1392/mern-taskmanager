52c2c4849441fd9e7c49521156b657b6c722400e
BanuPriya1392 <banusangee@gmail.com>
2026-03-16 20:19:37 +0530

    PERSONAL TASK MANAGER-MERN

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
+//dotenv is used to load environment variables from a .env file into process.env, and connectDB is a function that connects to the MongoDB database using Mongoose.
 dotenv.config();
 connectDB();
 
@@ -10,11 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: 'Server Error', error: err.message });
+  res.status(500).json({ message: "Server Error", error: err.message });
 });
 
 const PORT = process.env.PORT || 5000;
